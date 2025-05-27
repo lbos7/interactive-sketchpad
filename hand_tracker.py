76ca13f3e5cdb4557503e4ea211e32797521ccd0
Logan Boswell <loganstuartboswell@gmail.com>
2025-05-27 01:59:20 -0500

    Added variables for left and right hands; Added position getter

--- hand_tracker.py
+++ hand_tracker.py
@@ -6,12 +6,13 @@ import numpy as np
 class HandTracker:
 
     def __init__(self, static_mode=False, max_hands=2,
-                 min_detect_conf=0.7, min_track_conf=0.5):
+                 min_detect_conf=0.7, min_track_conf=0.5, min_lr_conf=0.8):
 
         self.static_mode = static_mode
         self.max_hands = max_hands
         self.min_detect_conf = min_detect_conf
         self.min_track_conf = min_track_conf
+        self.min_lr_conf = min_lr_conf
 
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(static_image_mode=self.static_mode,
@@ -23,15 +24,39 @@ class HandTracker:
 
     def detect_hands(self, img, visible_landmarks=True):
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        processed_img = self.hands.process(img_rgb)
+        self.img_h, self.img_w, _ = img_rgb.shape
+        self.results = self.hands.process(img_rgb)
 
-        if processed_img.multi_hand_landmarks:
-            for landmark in processed_img.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for i, landmarks in enumerate(self.results.multi_hand_landmarks):
+                handedness = self.results.multi_handedness[i].classification[0]
+                label = handedness.label
+                score = handedness.score
+
+                if score > self.min_lr_conf:
+                    if label == "Left":
+                        self.left_landmarks = landmarks
+                    elif label == "Right":
+                        self.right_landmarks = landmarks
+
                 if visible_landmarks:
                     self.mp_draw.draw_landmarks(img,
-                                                landmark,
+                                                landmarks,
                                                 self.mp_hands.HAND_CONNECTIONS)
 
         return img
 
-    # def 
+    def get_landmark_pos(self):
+        left_pos_list = []
+        right_pos_list = []
+
+        for landmark in self.left_landmarks:
+            row, col = int(landmark.y * self.img_h), int(landmark.x * self.img_w)
+            left_pos_list.append((row, col))
+
+        for landmark in self.right_landmarks:
+            row, col = int(landmark.y * self.img_h), int(landmark.x * self.img_w)
+            right_pos_list.append((row, col))
+
+        pos_dict = {"Left": left_pos_list, "Right": right_pos_list}
+        return pos_dict
