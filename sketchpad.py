84adb0acc9b98e32e4e02b217a0d7cd4e8f4f842
Logan Boswell <loganstuartboswell@gmail.com>
2025-05-27 19:46:32 -0500

    Added sketchpad file and got it to detect hands

---
+++ sketchpad.py
@@ -0,0 +1,53 @@
+import cv2
+import mediapipe as mp
+from hand_tracker import HandTracker
+from region import Region
+
+def create_buttons(starting_pos=(0, 0), button_size=(100, 100)):
+    starting_row = starting_pos[0]
+    starting_col = starting_pos[1]
+    button_list = [Region(starting_pos, button_size, (0, 0, 255)),
+                   Region((starting_row, starting_col + 1*button_size[1]), button_size, (255, 0, 0)),
+                   Region((starting_row, starting_col + 2*button_size[1]), button_size, (0, 255, 0)),
+                   Region((starting_row, starting_col + 3*button_size[1]), button_size, (0, 255, 255)),
+                   Region((starting_row, starting_col + 4*button_size[1]), button_size, (132, 42, 78)),
+                   Region((starting_row, starting_col + 5*button_size[1]), button_size, (255, 255, 255)),
+                   Region((starting_row, starting_col + 6*button_size[1]), button_size, (0, 0, 0)),
+                   Region((starting_row, starting_col + 7*button_size[1]), button_size, (128, 128, 128), text="Eraser"),
+                   Region((starting_row, starting_col + 8*button_size[1]), button_size, (128, 128, 128), text="Clear All")]
+    return button_list
+
+
+def main():
+
+    hand_tracker = HandTracker()
+    buttons = create_buttons()
+    sketchpad = Region((100, 0), (720 - 100, 1280), (255, 255, 255), transparency=0.0)
+
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 720)
+
+    while cap.isOpened():
+
+        success, frame = cap.read()
+        if not success:
+            print("Empty camera frame.")
+            break
+
+        # Flip the frame horizontally for selfie view
+        frame = cv2.flip(frame, 1)
+        hand_tracker.detect_hands(frame)
+
+        # Show the image
+        cv2.imshow('Interactive Sketchpad', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
